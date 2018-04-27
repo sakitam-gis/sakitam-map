93351c41ac76456b6fe037b352c194aa779b43f6
FDD <smileFDD@gmail.com>
2018-04-27 22:13:16 +0800

    vector layer

--- src/layer/VectorLayer.js
+++ src/layer/VectorLayer.js
@@ -1,7 +1,6 @@
 import Base from './Base';
 import { createCanvas, isEmpty } from '../utils';
 import ajax from '../utils/ajax';
-import { fromLonLat } from '../proj'
 
 class VectorLayer extends Base {
   constructor (options = {}) {
@@ -57,29 +56,133 @@ class VectorLayer extends Base {
   /**
    * re render
    */
-  render () {
+  render (options) {
     const map = this.getMap();
     const size = map.getSize();
     const context = this.getContext() || map.getContext();
     context.save();
     context.globalAlpha = this.getOpacity();
     context.fillStyle = '#08304b';
-    for (let i = 0; i < this._data.length; i++) {
-      const coords = this._data[i].geometry.coordinates[0];
-      for (let j = 0; j < coords.length; j++) {
-        const point = map.getPixelFromCoordinate(fromLonLat([coords[j][1], coords[j][0]]));
-        if (j === 0) {
-          context.beginPath();
-          context.moveTo(point[0], point[1]);
-        } else {
-          context.lineTo(point[0], point[1]);
-        }
+    context.save();
+    const data = this._data;
+    for (let key in options) {
+      context[key] = options[key];
+    }
+    if (options.bigData) {
+      context.save();
+      context.beginPath();
+      let item;
+      for (let i = 0, len = data.length; i < len; i++) {
+        item = data[i];
+        this._drawInternal(context, item, options);
       }
-      context.fill();
+      let type = options.bigData;
+      if (type === 'Point' || type === 'Polygon' || type === 'MultiPolygon') {
+        context.fill();
+        if ((item.strokeStyle || options.strokeStyle) && options.lineWidth) {
+          context.stroke();
+        }
+      } else if (type === 'LineString') {
+        context.stroke();
+      }
+      context.restore();
+    } else {
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        context.save();
+        if (item.fillStyle || item._fillStyle) {
+          context.fillStyle = item.fillStyle || item._fillStyle;
+        }
+        if (item.strokeStyle || item._strokeStyle) {
+          context.strokeStyle = item.strokeStyle || item._strokeStyle;
+        }
+        let type = item.geometry.type;
+        context.beginPath();
+        this._drawInternal(context, item, options);
+        if (type === 'Point' || type === 'Polygon' || type === 'MultiPolygon') {
+          context.fill();
+          if ((item.strokeStyle || options.strokeStyle) && options.lineWidth) {
+            context.stroke();
+          }
+        } else if (type === 'LineString') {
+          if (item.lineWidth || item._lineWidth) {
+            context.lineWidth = item.lineWidth || item._lineWidth;
+          }
+          context.stroke();
+        }
+        context.restore();
+      }
     }
     context.restore();
     if (this.getContext()) {
       map.getContext().drawImage(this.canvas_, 0, 0, size[0], size[1])
+    }
+  }
+
+  /**
+   * draw vector shape
+   * @param context
+   * @param data
+   * @param options
+   * @private
+   */
+  _drawInternal (context, data, options) {
+    let type = data.geometry.type;
+    let coordinates = data.geometry._coordinates || data.geometry.coordinates;
+    let symbol = options.symbol || 'circle';
+    switch (type) {
+      case 'Point':
+        let size = data._size || data.size || options._size || options.size || 5;
+        if (symbol === 'circle') {
+          if (options.bigData === 'Point') {
+            context.moveTo(coordinates[0], coordinates[1]);
+          }
+          context.arc(coordinates[0], coordinates[1], size, 0, Math.PI * 2);
+        } else if (symbol === 'rect') {
+          context.rect(coordinates[0] - size / 2, coordinates[1] - size / 2, size, size);
+        }
+        break;
+      case 'LineString':
+        for (let j = 0; j < coordinates.length; j++) {
+          let x = coordinates[j][0];
+          let y = coordinates[j][1];
+          if (j === 0) {
+            context.moveTo(x, y);
+          } else {
+            context.lineTo(x, y);
+          }
+        }
+        break;
+      case 'Polygon':
+        this._drawPolygon(context, coordinates);
+        break;
+      case 'MultiPolygon':
+        for (let i = 0; i < coordinates.length; i++) {
+          let polygon = coordinates[i];
+          this._drawPolygon(context, polygon);
+        }
+        context.closePath();
+        break;
+      default:
+        console.log('type' + type + 'is not support now!');
+        break;
+    }
+  }
+
+  /**
+   * draw polygon
+   * @param context
+   * @param coordinates
+   * @private
+   */
+  _drawPolygon (context, coordinates) {
+    for (let i = 0; i < coordinates.length; i++) {
+      let coordinate = coordinates[i];
+      context.moveTo(coordinate[0][0], coordinate[0][1]);
+      for (let j = 1; j < coordinate.length; j++) {
+        context.lineTo(coordinate[j][0], coordinate[j][1]);
+      }
+      context.lineTo(coordinate[0][0], coordinate[0][1]);
     }
   }
 }
